802ca5ea3e85a29b69925c05cd3297a1cf24c366
Alvandcode <alvandcode@atomicmail.io>
2026-09-11 18:45:31 +0330

    fix radar rotate import clash

--- app/src/main/java/com/alvand/securityscanner/MainActivity.kt
+++ app/src/main/java/com/alvand/securityscanner/MainActivity.kt
@@ -28,7 +28,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
