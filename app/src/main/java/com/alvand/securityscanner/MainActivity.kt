03b06a3ff5aa2277a4b6bd0cc0ca129be13199eb
Alvandcode <alvandcode@atomicmail.io>
2026-09-13 13:58:34 +0330

    fix: add missing rotate import for DrawScope in Radar composable

--- app/src/main/java/com/alvand/securityscanner/MainActivity.kt
+++ app/src/main/java/com/alvand/securityscanner/MainActivity.kt
@@ -36,6 +36,7 @@ import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
